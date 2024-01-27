41479e1d08582d3fe6b69b0c0ea3cfb29ecd6139
Amir Aini <amirhaini@gmail.com>
2024-01-27 06:17:59 +0000

    Finish github library

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -39,7 +39,7 @@ class Dev(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
 
     # Application definition
@@ -51,6 +51,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'gh',
     ]
 
     MIDDLEWARE = [
